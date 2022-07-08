123d78aaeffbb4877c7a41df729d4e941d0014d6
Edilson Morais <moraisbritoedilson@gmail.com>
2022-07-08 19:14:54 -0300

    adding bank.dart

--- lib/bank.dart
+++ lib/bank.dart
@@ -1,8 +1,26 @@
-import 'package:bank/account.dart';
-import 'package:bank/current_account.dart';
+import './account.dart';
+import './current_account.dart';
+import './special_account.dart';
 
 void main() {
-  Account c1 = CurrentAccount(agency: 232, number: 43643, name: "Denzel");
-  c1.deposit(1000);
-  print(c1);
+  Account c1 = CurrentAccount(
+    agency: 232,
+    number: 43643,
+    name: 'Current1',
+  );
+  Account c2 = CurrentAccount(
+    agency: 232,
+    number: 45342,
+    name: 'Current2',
+  );
+  Account s1 = SpecialAccount(
+    agency: 232,
+    number: 45342,
+    name: 'Special1',
+    limitAmmount: 1000.00,
+  );
+
+  print('$c1: ${c1.balance()}');
+  print('$c2: ${c2.balance()}');
+  print('$s1: ${s1.balance()}');
 }
