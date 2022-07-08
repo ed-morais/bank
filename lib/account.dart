15edf59ed67019db1d14ea3c4d49551b85f4f0d1
Edilson Morais <moraisbritoedilson@gmail.com>
2022-07-08 19:19:17 -0300

    updating account

--- lib/account.dart
+++ lib/account.dart
@@ -1,16 +1,41 @@
-import "./transaction.dart";
+import './transaction.dart';
 
 abstract class Account {
   final int agency;
   final int number;
   final String name;
-  // double _balance = 0.00; (Vamos calcular o saldo por meio das transações!)
+
   final List<Transaction> _transactions = [];
 
-  Account.open(
-      {required this.agency, required this.number, required this.name});
+  Account.open({
+    required this.agency,
+    required this.number,
+    required this.name,
+  });
 
-  double balance();
+  double balance() {
+    /*
+    double sum = 0.00;
+
+    for (int i = 0; i < _transactions.length; i++) {
+      sum += _transactions[i].value;
+    }
+
+    _transactions.forEach((trans) {
+      sum += trans.value;
+    });
+
+    for (var trans in _transactions) {
+      sum += trans.value;
+    }
+    */
+
+    return _transactions.fold<double>(
+      0.0,
+      (value, trans) => value + trans.value,
+    );
+  }
+
   void close();
   void deposit(double value);
   void withdraw(double value);
